ece635df704afc17d1cba89cadf0f230fa148340
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2024-11-08 18:07:25 +0545

    compleated crud operation for adding books

--- src/main/java/com/nextstep/multiauhtnticate/controller/AddUserBook.java
+++ src/main/java/com/nextstep/multiauhtnticate/controller/AddUserBook.java
@@ -10,12 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @Validated
@@ -39,6 +37,26 @@ public class AddUserBook {
 
         ApiResponse apiResponse=ApiResponse.builder().message("SuccessFullyAdded Book").statusCode(HttpStatus.OK.value()).build();
         return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
-
     }
+
+    @PreAuthorize("hasAuthority('ADMIN') || hasAuthority('LIBRARIAN')")
+    @GetMapping("/getAddedBook")
+    @Operation(summary = "Get Added Book",description ="This rout helps to get all added books")
+    public ResponseEntity<ApiResponse>getAllBooks(){
+        List list=addBookService.listOfAddedBook();
+        ApiResponse apiResponse=ApiResponse.<AddBook>builder().message("success").statusCode(HttpStatus.OK.value()).list( list).build();
+        return ResponseEntity.status(HttpStatus.OK).body(apiResponse) ;
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @DeleteMapping("/deleteById")
+    @Operation(summary = "Delete BookById",description = "This rout delete addedBook")
+    //if used path varible need send id in path
+    public ResponseEntity<ApiResponse>deleteBookById(@RequestParam("id") String id){
+        addBookService.deleteAddedBookById(id);
+        ApiResponse apiResponse=ApiResponse.builder().message("SuccessfullyDeleted").statusCode(HttpStatus.OK.value()).build();
+        return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+    }
+
 }
+
