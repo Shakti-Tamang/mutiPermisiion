6d3d73565614f4b05eb57549f1861831f5cb3341
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2024-11-09 13:28:10 +0545

    flyway migration added

---
+++ src/main/resources/db/migration/V1__add_user_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id VARCHAR(255) PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role_name VARCHAR(255),
+    user_role VARCHAR(255),
+    FOREIGN KEY (user_role) REFERENCES roles(id) -- Assuming roles is the name of the Role table
+);
