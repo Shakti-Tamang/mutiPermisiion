a9cfc55d8b4fa168835d44a468e52c3441282373
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2024-11-10 19:56:52 +0545

    create book checkout compleated

--- src/main/java/com/nextstep/multiauhtnticate/Repository/UserRepository.java
+++ src/main/java/com/nextstep/multiauhtnticate/Repository/UserRepository.java
@@ -2,12 +2,18 @@ package com.nextstep.multiauhtnticate.Repository;
 
 import com.nextstep.multiauhtnticate.Model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel,String> {
     public UserModel findByEmail(String email);
     public UserModel findByUsername(String username);
+    @Query("SELECT u FROM UserModel u WHERE u.id = :id")
+    public Optional<UserModel> findByIdExists(@Param("id") String id);
 
 
 }
