de62bd8967dadbf42045967669377165f1eb3fd7
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2024-11-22 22:15:30 +0545

    added some notes

---
+++ src/main/java/com/nextstep/multiauhtnticate/configuration/ApplicationCoTextSpring.java
@@ -0,0 +1,15 @@
+package com.nextstep.multiauhtnticate.configuration;
+
+public class ApplicationCoTextSpring {
+//    In the Spring Framework, the ApplicationContext is the central interface for configuring and accessing the Spring IoC (Inversion of Control) container. It is responsible for managing the lifecycle of beans and their dependencies in a Spring application.
+//
+//    Key Features of ApplicationContext
+//    Bean Management: It initializes, configures, and manages the lifecycle of Spring beans defined in the application.
+//    Dependency Injection: Facilitates the injection of dependencies into beans automatically.
+//    Resource Access: Provides convenient access to resources such as files and properties.
+//    Event Propagation: Supports event-driven programming with the ApplicationEventPublisher interface.
+//    Internationalization: Provides support for message sources to facilitate internationalization.
+//            Integration: Works seamlessly with other frameworks and technologies like Hibernate, JPA, and more.
+
+
+}
