86e445a213b34e99fd49a999d8eb3c60d716f76c
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2024-11-28 17:05:42 +0545

    custom method finder

--- src/main/java/com/nextstep/multiauhtnticate/Repository/CourseRepo.java
+++ src/main/java/com/nextstep/multiauhtnticate/Repository/CourseRepo.java
@@ -6,8 +6,15 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
+
+//custom method finder practice
 @Repository
 public interface CourseRepo extends JpaRepository<Courses,String> {
+
+
+//    using all kinds of custom method finder
+
 
 //    you cannot create the same cutom method which is already provided by jpa
 //    public List<Courses> findAllById(List<String>id);
